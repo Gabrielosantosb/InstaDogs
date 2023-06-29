b435bc6f53ca92ef9b5aeccb745e7faf0829d749
Gabrielosantosb <98432856+Gabrielosantosb@users.noreply.github.com>
2023-06-28 23:02:50 -0300

    Criacao rotas e svg

---
+++ src/Styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components"
+
+
+export const GlobalStyle = createGlobalStyle`
+    body{
+        margin: 0;
+        padding: 0;
+        color: #333;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+    img{
+        display: block;
+        max-width: 100%;
+    }
+    button{
+        display: block;
+        font-size: 1rem;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+
+`
