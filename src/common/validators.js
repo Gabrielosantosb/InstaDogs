ecbe7d4aae1a2612459dd571dd9e872a64175605
Gabrielosantosb <98432856+Gabrielosantosb@users.noreply.github.com>
2023-07-03 08:42:26 -0300

    endpoints

--- src/common/validators.js
+++ src/common/validators.js
@@ -1,35 +1,56 @@
-export const emailValidator = (email) => {
+export const usernameValidator = (username, setErrorMessage) => {
   let errors = {};
-  if (email == undefined) {
-    email = "";
+
+  if (username === undefined) {
+    username = "";
   }
-  var re =
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email != null && email.length > 0) {
-    if (re.test(email)) {
-        errors.result = true;
+
+  var re = /^[a-zA-Z0-9_]{3,16}$/;
+
+  if (username.length < 3) {
+    setErrorMessage('O usuário deve ser maior que 3 caracteres');
+    return false;
+  }
+
+  if (username !== null && username.length > 0) {
+    if (re.test(username)) {
+      errors.result = true;
     } else {
-        errors.result = false;
-      alert("E-mail Inválido");
+      errors.result = false;
     }
-  } else {
-    // errors.result = false;
-    alert("Digite seu e-mail.");
   }
+
   return errors;
 };
 
-export const passwordValidator = (password, newPassword, confirmPassword) => {
+
+export const passwordValidator = (password, newPassword, confirmPassword, setErrorMessage) => {
   let errors = {};
 
-  if (password == undefined) {
+  if (password === undefined) {
     password = "";
   }
 
-  if (password == newPassword) {
+  const re = /^.{2,}$/;
+
+  // if (password.length < 3) {
+  //   setErrorMessage('O usuário deve ser maior que 3 caracteres');
+  //   return false;
+  // }
+
+
+  if (password != null && password.length > 0) {
+    if (re.test(password)) {
+      errors.result = true;
+    } else {
+      errors.result = false;
+    }
+  }
+
+  if (password === newPassword) {
     errors.result = false;
     errors.errorMessage = "A nova senha deve ser diferente da senha atual";
-  } else if (newPassword == confirmPassword) {
+  } else if (newPassword === confirmPassword) {
     if (newPassword != null && newPassword.length >= 5) {
       errors.result = true;
     } else {
@@ -39,6 +60,29 @@ export const passwordValidator = (password, newPassword, confirmPassword) => {
   } else {
     errors.result = false;
     errors.errorMessage = "A senha e a confirmação da senha devem ser iguais.";
+  }
+
+  return errors;
+};
+
+export const emailValidator = (email) => {
+  let errors = {};
+  if (email === undefined) {
+    email = "";
+  }
+  var re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (email !== null && email.length > 0) {
+    if (re.test(email)) {
+      errors.result = true;
+    } else {
+      errors.result = false;
+      errors.errorMessage = "E-mail Inválido";
+    }
+  } else {
+    errors.result = false;
+    errors.errorMessage = "Digite seu e-mail.";
   }
 
   return errors;
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,51 +2,95 @@ import React, { useEffect, useState } from "react";
 import { ReactComponent as Loading } from "../../assets/carregando.svg";
 import { ButtonNav } from "../../styles/global";
 import axios from "axios";
-import { Button, FormContainer} from "./styles";
+import { Button, FormContainer } from "./styles";
 import { Input } from "../form/input";
-// import { useForm } from "../../Hooks/useForm";
+import { emailValidator, passwordValidator, usernameValidator } from "../../common/validators";
+import { Colors } from "../../styles/colors";
+import { TOKEN_POST, USER_GET } from "../../contants/endpoints";
 
-//'https://dogsapi.origamid.dev/json/jwt-auth'
 export const Login = () => {
-  
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false)
-  
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
+
+  // Timeout para set error
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorMessage('');
+    }, 3000);
+  }, [errorMessage]);
+
+  // Caso o usuario já esteja no localStorage
+
+    useEffect(() =>{
+      const token = window.localStorage.getItem('token')
+        if(token){
+          getUser(token)
+        }
+    },[])
+
+  const getUser = async(token) =>{
+    const {url, options} = USER_GET(token)
+    const response = await axios.get(url, options)
+    const {data} = response
+    console.log(data)
+  }
+  const validateFields = () => {
+    const usernameResult = usernameValidator(username, setErrorMessage);
+    const emailResult = emailValidator();
+    const passwordResult = passwordValidator(password, setErrorMessage);
+   
+    if (!usernameResult) {
+      return false;
+    }
+  
+    if (!passwordResult) {
+      return false;
+    }
+    
+  
+    return true;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+  
+    if (!validateFields()) {
+      return false;
+    }
+  
+    const { url, options } = TOKEN_POST({
+      username: username,
+      password: password,
+    });
+  
     try {
-      setLoading(true)
-      const response = await axios.post(
-        "https://dogsapi.origamid.dev/json/jwt-auth/v1/token",
-        {
-          username,
-          password,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      setLoading(false)
+      setLoading(true);
+      const response = await axios.post(url, {
+        username,
+        password,
+      }, options);
+      setLoading(false);
       const { data, status } = response;
-
+  
       console.log("Status:", status);
       console.log("Data:", data);
+  
+      // Armazena o token no localStorage
+      window.localStorage.setItem('token', data.token);
+      getUser(data.token)
     } catch (error) {
       console.error(error);
     }
   };
-
+  
   return (
     <>
       <FormContainer>
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
-
           <p>Usuário</p>
           <Input
             type="text"
@@ -59,10 +103,11 @@ export const Login = () => {
             onChange={({ target }) => setPassword(target.value)}
             value={password}
           />
-          
+
           <Button type="submit">Entrar</Button>
-        {loading ? <Loading/> : <></>}
+          {loading ? <Loading /> : null}
         </form>
+        <p style={{ color: Colors.red }}>{errorMessage}</p>
       </FormContainer>
       {/* <ButtonNav to="cadastro">Tela de cadastro</ButtonNav> */}
     </>
